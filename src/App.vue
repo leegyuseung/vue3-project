336bb75ff5e04fa1da2423c8d18a3461d64dd39c
leegyuseung <lgs4002@naver.com>
2022-12-04 19:33:52 +0900

    v-for 이용

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,9 @@
         <button type="submit" class="btn btn-primary">Add</button>
       </div>
     </form>
-    <div class="card mt-2">
+    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
       <div class="card-body p-2">
-        {{ todos[0].subject }}
-      </div>
-    </div>
-    <div class="card mt-2">
-      <div class="card-body p-2">
-        {{ todos[1].subject }}
+        {{ todo.subject }}
       </div>
     </div>
   </div>
