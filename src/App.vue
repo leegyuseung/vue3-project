edc8d0ebef9b88c4bab01044618e8e61d4a811d4
leegyuseung <lgs4002@naver.com>
2022-12-06 22:29:39 +0900

    검색 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search"
+    />
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
-
-    <div v-if="!todos.length">추가된 Todo가 없습니다.</div>
+    <div v-if="!filteredTodos.length">There is nothing to display</div>
     <TodoList
-      :todos="todos"
+      :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
@@ -13,7 +19,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
 
@@ -39,7 +45,26 @@ export default {
     const deleteTodo = (index) => {
       todos.value.splice(index, 1);
     };
-    return { toggleTodo, deleteTodo, addTodo, todos };
+
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    });
+
+    return {
+      toggleTodo,
+      deleteTodo,
+      addTodo,
+      todos,
+      searchText,
+      filteredTodos,
+    };
   },
 };
 </script>
