270586c776b47aa90ecbe1777614c3eb12ab8d7f
leegyuseung <lgs4002@naver.com>
2022-12-10 22:47:16 +0900

    To-Do 토글 db에서 하기

--- src/App.vue
+++ src/App.vue
@@ -63,10 +63,18 @@ export default {
       }
     };
 
-    const toggleTodo = (index) => {
-      console.log(todos.value[index]);
-      todos.value[index].completed = !todos.value[index].completed;
-      console.log(todos.value[index]);
+    const toggleTodo = async (index) => {
+      error.value = "";
+      const id = todos.value[index].id;
+      try {
+        await axios.patch("http://localhost:3000/todos/" + id, {
+          completed: !todos.value[index].completed,
+        });
+        todos.value[index].completed = !todos.value[index].completed;
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong.";
+      }
     };
 
     const deleteTodo = async (index) => {
