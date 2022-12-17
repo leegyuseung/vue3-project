0966c7f55919215aaf257940c2b19893cdda897b
leegyuseung <lgs4002@naver.com>
2022-12-17 22:26:15 +0900

    router-link를 이용한 navbar 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view></router-view>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Test Code</router-link>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/todos">Todos</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
 <script>
