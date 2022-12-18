faba1d594828e0fc70cedf1e2d15f94c0fefc588
leegyuseung <lgs4002@naver.com>
2022-12-18 20:52:31 +0900

    이벤트 버블링

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,14 +9,15 @@
           class="form-check-input"
           type="checkbox"
           :checked="todo.completed"
-          @change="toggleTodo(index)"
+          @change="toggleTodo(index, $event)"
+          @click.stop
         />
         <label class="form-check-label" :class="{ todo: todo.completed }">
           {{ todo.subject }}</label
         >
       </div>
       <div>
-        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">
+        <button class="btn btn-danger btn-sm" @click.stop="deleteTodo(index)">
           Delete
         </button>
       </div>
@@ -39,8 +40,8 @@ export default {
 
   setup(props, { emit }) {
     const router = useRouter();
-    const toggleTodo = (index) => {
-      emit("toggle-todo", index);
+    const toggleTodo = (index, event) => {
+      emit("toggle-todo", index, event.target.checked);
     };
 
     const deleteTodo = (index) => {
