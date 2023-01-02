36e5b726a881fc2aa9c48cf8f5e5f974706ec0fa
leegyuseung <lgs4002@naver.com>
2023-01-02 22:24:14 +0900

    toRefs

--- src/pages/indexVue.vue
+++ src/pages/indexVue.vue
@@ -1,9 +1,19 @@
 <template>
   <div>Home Page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">Add</button>
 </template>
 
 <script>
-export default {};
+import { useCount } from "@/composables/count";
+export default {
+  setup() {
+    const { count } = useCount();
+    console.log(count);
+
+    return { count };
+  },
+};
 </script>
 
 <style></style>
